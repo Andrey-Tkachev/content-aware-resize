2b43366c1588e4a0fbf9d5cb6ff716a2c18d801c
Andrey Tkachev <tka4.andr@yandex.ru>
2017-07-06 20:38:05 +0300

    not working state

--- include/core.h
+++ include/core.h
@@ -38,26 +38,112 @@ namespace core {
         MatWrp& operator= (MatWrp&& other);
     };
 
+    void calc_dynamics(const MatWrp& in, MatWrp& dynamics) {
+        for (int i = 0; i < in.hieght(); ++i) {
+            dynamics.at<double>(i, 0) = in.at<double>(i, 0);
+        }
+
+        for (int curr_col = 1; curr_col < in.width(); ++curr_col) {
+            for (int curr_row = 0; curr_row < in.hieght(); ++curr_row) {
+                double curr_min = dynamics.at<double>(curr_row, curr_col - 1) + in.at<double>(curr_row, curr_col);
+                for (int delta = -1; delta <= 1; ++delta) {
+                    if (delta + curr_row < in.hieght() && delta + curr_row >= 0) {
+                        if (curr_min > in.at<double>(curr_col, curr_row) +
+                                       dynamics.at<double>(curr_row + delta, curr_col - 1)) {
+                            curr_min = in.at<double>(curr_row, curr_col) +
+                                       dynamics.at<double>(curr_row + delta, curr_col - 1);
+                        }
+                    }
+                }
+                dynamics.at<double>(curr_row, curr_col) = curr_min;
+            }
+        }
+    }
+
+    template <typename TFilter>
+    PVec low_energy_path(const MatWrp& in, const TFilter& filter) {
+        MatWrp grad;
+        grad.set_shape(in);
+        filter(in.mat, grad.mat);
+        MatWrp dynamics(in.hieght(), in.width(), in.mat.type());
+        calc_dynamics(grad, dynamics);
+        double min = dynamics.at<double>(0, in.width() - 1);
+        int min_i = 0;
+        for (int i = 0; i < dynamics.hieght(); ++i) {
+            if (min > dynamics.at<double>(i, in.width() - 1)) {
+                min = dynamics.at<double>(i, in.width() - 1);
+                min_i = i;
+            }
+        }
+        int curr_row = min_i;
+        int curr_col = in.width() - 1;
+        PVec path;
+        path.emplace_back(curr_col, curr_row);
+        while (curr_col > 0) {
+            int suitable_delta = 0;
+            double curr_min = 1e8;
+            for (int delta = -1; delta <= 1; ++delta) {
+                if (delta + curr_row < in.hieght() && delta + curr_row >= 0) {
+                    if (curr_min > dynamics.at<double>(curr_row + delta, curr_col)) {
+                        curr_min = dynamics.at<double>(curr_row + delta, curr_col);
+                        suitable_delta = delta;
+                    }
+                }
+            }
+            curr_row += suitable_delta;
+            --curr_col;
+            path.emplace_back(curr_col, curr_row);
+        }
+        return path;
+    }
+
+
+    void remove_row(PVec& points, MatWrp& from) {
+        for (int i = 0; i != from.width(); ++i) {
+            int delta = 0;
+            for (int j = 0; j != from.hieght(); ++j) {
+                if (points[i].y == j) {
+                    delta = -1;
+                    continue;
+                }
+                from.at<cv::Vec3b>(j + delta, i) = from.at<cv::Vec3b>(j, i);
+            }
+        }
+        from = from(cv::Range(0, from.hieght() - 1), cv::Range(0, from.width()));
+    }
+
+    // Remove/add k rows to the image
+    template <typename TFilter> // Class with overrided operator()
+    void remove_rows(MatWrp& in, int k, const TFilter& filter) {
+        for (int l = 0; l != k; ++l) {
+            auto pixels_to_remove = low_energy_path(in, filter);
+            remove_row(pixels_to_remove, in);
+        }
+    }
+
     // Change image size to desirable
     // shrink_to_fit of image (640 x 480) with new_size (600 x 500) returns image (600 x 480)
     // expand_to_fit of image (640 x 480) with new_size (600 x 500) returns image (640 x 500)
     template <typename TFilter>
-    void shrink_to_fit(const cv::Mat in, cv::Mat out, const cv::Size& new_size, const TFilter& filter);
-    template <typename TFilter>
-    void expand_to_fit(const cv::Mat in, cv::Mat out, const cv::Size& new_size, const TFilter& filter);
+    void shrink_to_fit(const cv::Mat& in, cv::Mat& out, const cv::Size& new_size, const TFilter& filter) {
+        cv::Size in_size = in.size();
+        MatWrp in_wrp(in.clone());
+        MatWrp out_wrp;
+        if (in_size.height > new_size.height) {
+            remove_rows(in_wrp, in_size.height - new_size.height, filter);
+        }
+        if (in_size.width > new_size.width) {
+            in_wrp.transpose();
+            remove_rows(in_wrp, in_size.width - new_size.width, filter);
+        }
+        out = in_wrp.mat;
+    }
 
-    // Remove(add) k rows of dots from(to) matrix "in"
     template <typename TFilter>
-    void remove_rows(const MatWrp& in, MatWrp& out, int k, const TFilter& filter);
-    template <typename TFilter>
-    void    add_rows(const MatWrp& in, MatWrp& out, int k, const TFilter& filter);
+    void expand_to_fit(const cv::Mat& in, cv::Mat& out, const cv::Size& new_size, const TFilter& filter) {
+        // TODO
+    }
 
-    void remove_row(PVec& points, MatWrp& from);
-    void    add_row(PVec& points, MatWrp& from);
-
-    template <typename TFilter>
-    PVec low_energy_path(const MatWrp& in, const TFilter& filter);
-    MatWrp calc_dynamics(const MatWrp& in);
 }
 
 #endif //CONTENTAWARERESIZE_CORE_H
---
+++ tests/test_build/main.cpp
@@ -0,0 +1,39 @@
+#include "core.h"
+#include <opencv2/opencv.hpp>
+#include "iostream"
+
+void gradient_xy(cv::Mat &in, cv::Mat &out, int x_ord,
+                 int y_ord) {
+    int scale = 1;
+    int delta = 0;
+    int ddepth = CV_16S;
+    int ker_size = 3;
+    cv::Sobel(in, out, ddepth, x_ord, y_ord, ker_size, scale, delta, cv::BORDER_DEFAULT);
+    cv::convertScaleAbs(out, out);
+}
+
+void gradient(const cv::Mat& in, cv::Mat& out) {
+    int blur_size = 3;
+    int sigma = 4;
+    cv::Mat src_gray = in.clone();
+    cvtColor(src_gray, src_gray, CV_BGR2GRAY);
+    cv::GaussianBlur(src_gray, src_gray, cv::Size(blur_size, blur_size), sigma, sigma,
+                     cv::BORDER_DEFAULT);
+    cv::Mat grad_x, grad_y;
+    gradient_xy(src_gray, grad_x, 1, 0);
+    gradient_xy(src_gray, grad_y, 0, 1);
+    addWeighted(grad_x, 0.5, grad_y, 0.5, 0, out);
+}
+
+int main() {
+    cv::Mat image;
+    image = cv::imread("test_images/duck_640x640.jpg", CV_LOAD_IMAGE_COLOR);
+
+    cv::Mat new_im;
+    core::shrink_to_fit(image, new_im, cv::Size(540, 640), gradient);
+    cv::imshow("image", new_im);
+    cv::imshow("original", image);
+    if (cv::waitKey()){
+        return 0;
+    }
+}
