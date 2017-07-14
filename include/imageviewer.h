f33db13762c6920630ea486b1f149028eb04f1ee
Andrey Tkachev <tka4.andr@yandex.ru>
2017-07-15 02:32:46 +0300

    shit optimization

--- include/imageviewer.h
+++ include/imageviewer.h
@@ -11,9 +11,12 @@
 #include "core.h"
 #include "filters.h"
 
-    QImage Mat2QImage(cv::Mat& src)
+QImage Mat2QImage(cv::Mat const& src)
 {
-    QImage dest((const uchar *) src.data, src.cols, src.rows, src.step, QImage::Format_RGB888);
+    //cv::Mat temp; // make the same cv::Mat
+    //cvtColor(src, temp,CV_BGR2RGB); // cvtColor Makes a copt, that what i need
+    auto temp = src;
+    QImage dest((const uchar *) temp.data, temp.cols, temp.rows, temp.step, QImage::Format_RGB888);
     dest.bits(); // enforce deep copy, see documentation
     // of QImage::QImage ( const uchar * data, int width, int height, Format format )
     return dest;
@@ -23,7 +26,9 @@ cv::Mat QImage2Mat(QImage const& src)
 {
     cv::Mat tmp(src.height(),src.width(),CV_8UC3,(uchar*)src.bits(),src.bytesPerLine());
     cv::Mat result; // deep copy just in case (my lack of knowledge with open cv)
-    cvtColor(tmp, result,CV_BGR2RGB);
+    //cvtColor(tmp, result,CV_BGR2RGB);
+    cvtColor(tmp, result, CV_RGB2BGR);
+    cvtColor(result, result, CV_BGR2RGB);
     return result;
 }
 
